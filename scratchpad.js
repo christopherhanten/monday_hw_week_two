abaad10eaf8e4949633b52a2142bd3e3b53d2214
Your Name <controlsltrapidfire@gmail.comm>
2017-12-11 23:34:05 -0600

    3rd commit - performed operations on the crazy object.

--- scratchpad.js
+++ scratchpad.js
@@ -60,10 +60,49 @@
 //2.console.log(rainbow[4]);
 
 //3.
-let christopher = {
-      faveFood: "Pizza!",
-      aHobby: "Making beats",
-      currentTown: "ATX-78752",
-      faveDataType: "object, duh."
-};
-    console.log(christopher.aHobby);
+// let christopher = {
+//       faveFood: "Pizza!",
+//       aHobby: "Making beats",
+//       currentTown: "ATX-78752",
+//       faveDataType: "object, duh."
+// };
+//4.
+    // console.log(christopher.aHobby);
+
+////////////////Crazy Object!/////////////////////////
+
+const crazyObject = {
+  taco: [{meat: 'steak',
+         cheese: ['panela', 'queso', 'chihuahua']},
+         {meat: 'chicken',
+          salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+        ],
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    characters: [{
+      name: "Jeff",
+      occupation: "manager"
+    },
+    {
+      name: "funkhauser",
+      occupation: "tv dude"
+    },
+    {
+      name: "susie",
+      occupation: "jeffs wife",
+      favourtieHobby: "Swearing at Larry and Jeff"
+    },
+    ]
+  }
+}
+//1.
+//console.log(crazyObject.taco[1].salsa[5]);
+//2.
+//console.log(crazyObject.larry.quotes[0]);
+//3.
+//console.log(crazyObject.larry.characters[2].favourtieHobby);
+//4.
+//console.log(crazyObject.larry.nicknames[1]);
+//5.
+console.log(crazyObject.larry.characters[1].name);
